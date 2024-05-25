ce07103c7dbd81a4c441dcacd3ef866c5ea33aea
Alemha <37380232+Alperemrehas@users.noreply.github.com>
2024-05-25 22:02:02 +0300

    Azure Webapp

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -1,7 +1,9 @@
 package tr.edu.metu.sm703;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -10,5 +12,14 @@ public class HomeController {
     @Get
     public Map<String, Object> index() {
         return Collections.singletonMap("message", "Hello World");
+    }
+
+    @Get("/add")
+    public Map<String, Object> add(@QueryValue int a, @QueryValue int b) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("a", a);
+        response.put("b", b);
+        response.put("sum", a + b);
+        return response;
     }
 }
